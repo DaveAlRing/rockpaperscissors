a5ada947f7657b143b2435c75f14fc520c49e8d8
Dave Ring <davealring@gmail.com>
2024-07-10 14:19:15 -0400

    Added script for computer RPS choice

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log("Hello World");
+
+
+function getComputerChoice(){
+    let randomValue = Math.floor(Math.random() * 3);
+    
+    if(randomValue == 0) {
+        return "Rock"
+    } else if (randomValue == 1) {
+        return "Paper"
+    } else {
+        return "Scissors"
+    };
+}
+
+let choice = getComputerChoice();
+console.log(choice);
